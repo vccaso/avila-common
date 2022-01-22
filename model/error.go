45b020b7b6937000df6a1ed2cc0b7d7a6b08fe75
vccaso <vccaso@scartarissolutions.com>
2022-01-22 10:55:55 -0500

    error model added and some checks with log features

---
+++ model/error.go
@@ -0,0 +1,61 @@
+package model
+
+import (
+	"encoding/json"
+	"io"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
+
+// swagger:model
+type Error struct {
+	// the id for the product
+	//
+	// required: true
+	// min: 1
+	Id int64 `json:"id"`
+	// Time for the error
+	//
+	// required: true
+	Error_time time.Time `json:"error_time" validate:"required"`
+	// application where the error was thrown
+	//
+	// required: true
+	App string `json:"app" validate:"required"`
+	// Error message
+	//
+	// required: true
+	Message string `json:"message" validate:"required"`
+	// UUID with the gateway session
+	//
+	// required: true
+	Gateway_session string `json:"gateway_session" validate:"required"`
+}
+
+// swagger:model
+type Errors []*Error
+
+// json converters
+
+func (u *Errors) ToJson(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(u)
+}
+
+func (u *Error) ToJson(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(u)
+}
+
+func (u *Error) FromJson(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(u)
+}
+
+// validators
+
+func (u *Error) Validate() error {
+	validate := validator.New()
+	return validate.Struct(u)
+}
--- util/errors.go
+++ util/errors.go
@@ -1,7 +1,48 @@
 package util
 
-func Check(e error) {
+import (
+	"log"
+	"os"
+)
+
+// Info writes logs in the color blue with "INFO: " as prefix
+var Info = log.New(os.Stdout, "\u001b[34mINFO: \u001B[0m", log.LstdFlags|log.Lshortfile)
+
+// Warning writes logs in the color yellow with "WARNING: " as prefix
+var Warning = log.New(os.Stdout, "\u001b[33mWARNING: \u001B[0m", log.LstdFlags|log.Lshortfile)
+
+// Error writes logs in the color red with "ERROR: " as prefix
+var Error = log.New(os.Stdout, "\u001b[31mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile)
+
+// Debug writes logs in the color cyan with "DEBUG: " as prefix
+var Debug = log.New(os.Stdout, "\u001b[36mDEBUG: \u001B[0m", log.LstdFlags|log.Lshortfile)
+
+func CheckPanic(e error) {
 	if e != nil {
 		panic(e.Error())
+	}
+}
+
+func CheckError(e error) {
+	if e != nil {
+		Error.Println(e.Error())
+	}
+}
+
+func CheckWarning(e error) {
+	if e != nil {
+		Warning.Println(e.Error())
+	}
+}
+
+func CheckInfo(e error) {
+	if e != nil {
+		Info.Println(e.Error())
+	}
+}
+
+func CheckDebug(e error) {
+	if e != nil {
+		Debug.Println(e.Error())
 	}
 }

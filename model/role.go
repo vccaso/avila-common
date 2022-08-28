c8c1abdedae586483572fa6583b30d6cca942299
winncod <winncod@gmail.com>
2022-08-27 23:26:25 -0500

    Added new helpers to scan database rows

--- model/role.go
+++ model/role.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"database/sql"
 	"encoding/json"
 	"io"
 
@@ -33,6 +34,27 @@ func (u *Role) ToJson(w io.Writer) error {
 func (u *Role) FromJson(r io.Reader) error {
 	e := json.NewDecoder(r)
 	return e.Decode(u)
+}
+
+// function to scan Role object from database single row
+func (u *Role) FromRow(row *sql.Rows) error {
+
+	err := row.Scan(&u.Id, &u.Name, &u.Isactive, &u.Isdeleted)
+	return err
+}
+
+// function to scan Role object from database rows
+func (u *Roles) FromRows(rows *sql.Rows) error {
+
+	for rows.Next() {
+		r := new(Role)
+		err := r.FromRow(rows)
+		if err != nil {
+			return err
+		}
+		*u = append(*u, r)
+	}
+	return nil
 }
 
 // validators

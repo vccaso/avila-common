a05d70b28dd4be02379a87cd6a66fa1746de065b
winncod <winncod@gmail.com>
2022-09-01 18:13:03 -0500

    Created a new function to handle API Errors: Send errors to  API-Logs and write HttpCode to header

---
+++ util/logs.go
@@ -0,0 +1,45 @@
+package util
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"avila-common/model"
+)
+
+var logs_host string = "http://" + os.Getenv("LOGS_HOST") + ":8093/api"
+
+type ApiError struct {
+	StatusCode int
+
+	Message string
+}
+
+func HandleApiError(e error, appName string, httpCode int, errorMessage string, response http.ResponseWriter) {
+
+	if e != nil {
+		error := model.Error{}
+		error.App = appName
+		error.Error_time = time.Now()
+		error.Message = e.Error()
+		error.Gateway_session = "TODO"
+
+		reqBodyBytes := new(bytes.Buffer)
+		json.NewEncoder(reqBodyBytes).Encode(error)
+		resp, err := http.Post(logs_host+"/error", "application/json", bytes.NewBuffer(reqBodyBytes.Bytes()))
+		CheckError(err)
+		if resp != nil {
+			defer resp.Body.Close()
+		}
+	}
+	if response != nil {
+
+		response.WriteHeader(httpCode)
+		jsonError, _ := json.Marshal(&ApiError{httpCode, errorMessage})
+		fmt.Fprintf(response, "%v", string(jsonError))
+	}
+}
